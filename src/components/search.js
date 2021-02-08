91f4841aa30161631483b1bea21221f1184f814f
João <joaopjk48@gmail.com>
2021-02-08 14:43:05 -0300

    Add projeto

---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div className="seach">
+            <input type="search" placeholder="Digite o nome do usuário no github"/>
+        </div>
+    )
+}
+
+export default Search;
---
+++ src/components/userInfo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const UserInfo = () => {
+    return (
+        <div className="user-info">
+            <img src="" alt=""/>
+            <h1>
+                <a href="#"> João Cicero</a>
+            </h1>
+            <div className="repos-info">
+                <ul>
+                    <li>Repositorios:</li>
+                    <li>Seguidores:</li>
+                    <li>Seguindo:</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default UserInfo;
